17bb03612100ce43aa364026f378edd9bb1b2c85
SAMZID KHAN <65766247+samzid@users.noreply.github.com>
2022-10-09 15:19:29 +0600

    Add files via upload
    How to remove style in Js

---
+++ index .js
@@ -0,0 +1,12 @@
+function addStyle(){
+
+    var myStyle =document.querySelector("#paraId",);
+    myStyle.classList.add("paraStyle");
+}
+
+
+function remove(){
+
+    var myStyle = document.querySelector("#paraId");
+    myStyle.classList.remove("paraStyle");
+}
